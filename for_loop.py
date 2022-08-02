5617f37e7486c1918f9333e981aa401ff66c1ec3
Adidev <adidev1010@gmail.com>
2022-08-02 09:11:25 +0530

    Initial commit

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+# looping through a list.
+
+crushes = ['rishu', 'akshara', 'shraddha', 'tanya']
+
+for crush in crushes:         # this is the syntax of for loop in python.
+    print(f"{crush.title()} was my crush.")
+
+
+# AVOID INDENTATION ERRORS...
+
---
+++ practice_loop.py
@@ -0,0 +1,9 @@
+#exercise 4-1
+
+pizzas=['Neapolitan Pizza','Chicago Pizza','Pizza Marinara']
+for pizza in pizzas:
+    print(f"I love {pizza} very much.")
+    
+    print(f"I love every kinda pizza.")
+
+print('Thanks to having me any of the pizzas. ')
